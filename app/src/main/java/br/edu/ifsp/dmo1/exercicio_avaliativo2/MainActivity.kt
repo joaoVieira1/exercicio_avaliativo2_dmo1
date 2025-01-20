18be064ba1b68f6c5cf5d9fe3359bd29470af941
João Pedro da Silva Vieira AQ3022366 <vieira.joao1@aluno.ifsp.edu.br>
2025-01-20 12:03:50 -0300

    first configs

---
+++ app/src/main/java/br/edu/ifsp/dmo1/exercicio_avaliativo2/MainActivity.kt
@@ -0,0 +1,17 @@
+package br.edu.ifsp.dmo1.exercicio_avaliativo2
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import br.edu.ifsp.dmo1.exercicio_avaliativo2.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+}
